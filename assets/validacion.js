91acd868ba216037e6fad12641e8d7c4059e8271
Cristian Lima <cristiannlima2@gmail.com>
2025-01-21 07:43:15 -0600

    Login realizado

---
+++ assets/validacion.js
@@ -0,0 +1,19 @@
+const credenciales = {
+    correoValido: "alexxo@gmail.com",
+    contraValida: "0000"
+};
+
+const boton = document.getElementById("boton");
+const error = document.getElementById("error");
+
+boton.addEventListener("click", () => {
+    const correo = document.getElementById("email").value.trim();
+    const contra = document.getElementById("contra").value.trim();
+    const contraInput = document.getElementById("contra");
+
+    (correo === credenciales.correoValido && contra === credenciales.contraValida)
+        ? window.location.href = "./Contactos/contacto.html"
+        : error.textContent = "Correo o contraseña incorrecta!";
+    contraInput.value = "";
+
+});
